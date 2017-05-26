70f28f96e4af9755bcb43f80ccb5ee99c94133f8
Jarred Parr <jparr721@gmail.com>
2017-05-25 21:01:58 -0400

    Working on account page

--- BadMeat/src/pages/account-page/account-page.ts
+++ BadMeat/src/pages/account-page/account-page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the AccountPage page.
@@ -13,12 +14,66 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'account-page.html',
 })
 export class AccountPage {
+  isLocationOn: boolean = true;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alert: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AccountPage');
+  }
+
+  areYouSure() {
+    if(this.isLocationOn == false){
+      let promptCheck = this.alert.create({
+        title: 'Are you sure?',
+        message: 'This saves battery, but restricts some features!',
+        buttons: [
+          {
+            text: 'Cancel',
+            handler: () => {
+              console.log("Location is on");
+              this.isLocationOn = true;
+            }
+          },
+          {
+            text: 'Yes',
+            handler: () => {
+              this.isLocationOn = false;
+            }
+          }
+        ]
+      });
+      promptCheck.present();
+    } else {
+      console.log("Location is not turned on!");
+    }
+  }
+
+  changePasswordConfirm() {
+    let passwordConfirm = this.alert.create({
+      title: 'Change password?',
+      message: 'A link will be sent to your email',
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: () => {
+            console.log("No password reset");
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.changePassword();
+          }
+        }
+      ]
+    });
+    passwordConfirm.present();
+  }
+
+  changePassword(){
+    //TODO - Connect to databse and send an email
   }
 
 }
