cb5232749786f8e4349edf867bfab7789457a376
Jarred Parr <jparr721@gmail.com>
2017-07-10 15:44:12 -0400

    Last minute changes before I leave. Adding to FoodItemController class

--- badmeat-react-prototype/API/Badmeat.API/Controllers/FoodItemController.cs
+++ badmeat-react-prototype/API/Badmeat.API/Controllers/FoodItemController.cs
@@ -14,7 +14,31 @@ namespace Badmeat.API.Controllers
 
         public FoodItemController(FoodListItemsContext context)
         {
-            
+            _context = context;
+
+            if (_context.FoodListItems.Count() == 0)
+            {
+                _context.FoodListItems.Add(new FoodListItems { FoodName = "Example Food"});
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<FoodListItems> GetAll()
+        {
+            return _context.FoodListItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetFood")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.FoodListItems.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound("Data not found");
+            }
+
+            return new ObjectResult(item);
         }
     }
 }
