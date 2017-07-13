3ead94499a8e83003ce943cec751e4f64c9e30d3
Jarred Parr <jparr721@gmail.com>
2017-07-13 12:00:47 -0400

    camera isn't working *sadface*

--- badmeat-react-prototype/App/app/components/camera/camera.js
+++ badmeat-react-prototype/App/app/components/camera/camera.js
@@ -1,70 +1,21 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, Image, ScrollView, TouchableHighlight } from 'react-native';
 import { Sheet } from '../../styles';
-import Cam from 'react-native-camera';
 
 export default class Camera extends Component {
 
-    getInitialState() {
-        return {
-            cameraType: Cam.constants.Type.back
-        }
-    }
+    // getInitialState() {
+    //     return {
+    //         cameraType: Cam.constants.Type.back
+    //     }
+    // }
 
     render(){
-        return (
-           <Cam
-                ref="cam"
-                style={Sheet.contianerAdjust}
-                type={this.state.cameraType}>
-                <View style={styles.buttonBar}>
-                    <TouchableHighlight style={styles.button} onPress={this._switchCamera}>
-                        <Text style={styles.buttonText}>Flip</Text>
-                    </TouchableHighlight>
-                    <TouchableHighlight style={styles.button} onPress={this._takePicture}>
-                        <Text style={styles.buttonText}>Take</Text>
-                    </TouchableHighlight>
-                </View>
-            </Cam>
-        );
-    }
+        return(
+            <View style={Sheet.contianerAdjust}>
+                <Text>Fuck the niggers</Text>
+            </View>
+        )
 
-    _switchCamera() {
-        let state = this.state;
-        state.cameraType = state.cameraType === Cam.constants.Type.back ? Cam.constants.Type.front : Cam.constants.Type.back;
-        this.setState(state);
-    }
-
-    _takePicture() {
-        this.refs.cam.capture((err, data) => {
-            console.log(err, data);
-        });
     }
 }
-
-let styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "transparent",
-    },
-    buttonBar: {
-        flexDirection: "row",
-        position: "absolute",
-        bottom: 25,
-        right: 0,
-        left: 0,
-        justifyContent: "center"
-    },
-    button: {
-        padding: 10,
-        color: "#FFFFFF",
-        borderWidth: 1,
-        borderColor: "#FFFFFF",
-        margin: 5
-    },
-    buttonText: {
-        color: "#FFFFFF"
-    }
-});
