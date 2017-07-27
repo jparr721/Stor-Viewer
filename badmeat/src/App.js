9c3995015a4c7c2b492c80b5171c5af84aabbafb
Jarred Parr <jparr721@gmail.com>
2017-07-27 19:00:43 -0400

    initial push

---
+++ badmeat/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import Dashboard from './components/Dashboard';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Dashboard />
+      </View>
+    );
+  }
+}
+
+export default App;
---
+++ badmeat/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Dashboard Component</Text>
+      </View>
+    );
+  }
+}
+
+export default Dashboard;
