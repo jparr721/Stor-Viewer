0b5a44335d5e67a3bd784117b9a7ea9f17b05ce6
deonj197 <deonj197@gmail.com>
2017-08-11 21:23:08 -0400

    Added JSON file with Pantry test data. Rebuilt Pantry.

--- Badmeat/src/components/Pantry.js
+++ Badmeat/src/components/Pantry.js
@@ -2,43 +2,37 @@ import React, {Component} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 import {Card, SmallCard, Header } from './common'
 import {SmallHeader} from './common/SmallHeader'
+const pantryData = require( '../../dummy_data/dummy_data_1.json');
 class Pantry extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fullPantryList: [],
+            expiringItems: [],
+            recentlyAdded: []
+        };
+    }
+
     render() {
+        this.fetchAllFood();
+        this.fetchExpiring();
+        this.fetchNewFood();
         return (
             <View style={styles.container}>
                 <Header headerText='Pantry'/>
+
                 {/*Expiring food view (May add horizontal scrolling)*/}
                 <SmallHeader headerText="Expiring Soon"/>
                 <View style={styles.row}>
-                    <SmallCard>
-                        <Text>Small Card</Text>
-                    </SmallCard>
-                    <SmallCard>
-                        <Text>Small Card</Text>
-                    </SmallCard>
-                    <SmallCard>
-                        <Text>Small Card</Text>
-                    </SmallCard>
-                    <SmallCard>
-                        <Text>Small Card</Text>
-                    </SmallCard>
+                    {this.state.expiringItems}
                 </View>
+
                 {/*Recently added food view (May add horizontal scrolling)*/}
                 <SmallHeader headerText="Recently Added"/>
                 <View style={styles.row}>
-                    <SmallCard>
-                        <Text>Small Card</Text>
-                    </SmallCard>
-                    <SmallCard>
-                        <Text>Small Card</Text>
-                    </SmallCard>
-                    <SmallCard>
-                        <Text>Small Card</Text>
-                    </SmallCard>
-                    <SmallCard>
-                        <Text>Small Card</Text>
-                    </SmallCard>
+                    {this.state.recentlyAdded}
                 </View>
+
                 <View>
                     <Button
                         onPress={() => this.handleViewAll()}
@@ -47,6 +41,36 @@ class Pantry extends Component {
                 </View>
             </View>
         );
+    }
+
+    fetchAllFood () {
+        for (let i in pantryData.fullPantry) {
+            this.state.fullPantryList.push(
+                <SmallCard key={i}>
+                    <Text>{i}</Text>
+                </SmallCard>
+            )
+        }
+    }
+
+    fetchExpiring () {
+        for (let i in pantryData.expiringFood) {
+            this.state.expiringItems.push(
+                <SmallCard key={pantryData.expiringFood[i].name}>
+                    <Text>{pantryData.expiringFood[i].name}</Text>
+                </SmallCard>
+            )
+        }
+    }
+
+    fetchNewFood () {
+        for (let i in pantryData.newPantryItems) {
+            this.state.recentlyAdded.push(
+                <SmallCard key={pantryData.newPantryItems[i].name}>
+                    <Text>{pantryData.newPantryItems[i].name}</Text>
+                </SmallCard>
+            )
+        }
     }
 
     // Place holder for button handler
