7d9fa31bb21db2c4559d2d2c816bca940a1d884f
Jarred Parr <jparr721@gmail.com>
2017-08-26 15:11:48 -0400

    Setup app for heroku

--- Badmeat/server/db/mongoose.js
+++ Badmeat/server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 // Set it up to use promises
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Badmeat');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Badmeat');
 
 module.exports = { mongoose };
