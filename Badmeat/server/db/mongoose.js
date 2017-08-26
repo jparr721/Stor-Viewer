b66da7f15fc5b407d30d5149e1973acd044ab575
Jarred Parr <jparr721@gmail.com>
2017-08-26 16:24:34 -0400

    Removed server to make hosting easy

--- Badmeat/server/db/mongoose.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-// Set it up to use promises
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Badmeat');
-
-module.exports = { mongoose };
