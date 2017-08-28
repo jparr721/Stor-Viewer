034aca2086db021b74f41999ac8a72551e5f243f
deonj197 <deonj197@gmail.com>
2017-08-27 21:48:26 -0400

    Implimented the popup screen for all HeaderButtons, with page specific content support.
    Moved the logic for displaying the Modal popup screen into the HeaderButton object definition. The header class uses the icon name passed in for the HeaderButton to determine which content to display on the corresponding Modal popup screen. It now works for every header button. Repurposed the NewEntryDialogue object as a container for page specific content on the the add new entry page. Next objective for me will be adding in redux support in several places. And after that, implimenting our API in pantry.

--- Badmeat/src/components/common/HeaderButton.js
+++ Badmeat/src/components/common/HeaderButton.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import { TouchableOpacity, StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import { TouchableOpacity, StyleSheet,View, Text, Modal} from 'react-native';
 import { Icon } from 'react-native-elements';
+import {NewEntryDialogue} from "./NewEntryDialogue";
 
-const styles = StyleSheet.create({
-  button: {
-    marginRight:10
+class HeaderButton extends Component {
+  state= {
+    modalVisible: false
+  };
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
   }
-});
 
-const HeaderButton = (props) => {
-  const { onPress, children } = props;
+  render() {
+    return (
+      <View style={{marginRight: 10 }}>
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {console.log("Modal has been closed")}}
+        >
+          <View>
+            <View style = {{ paddingTop: 10, paddingBottom: 10, backgroundColor:'#42a5f5'}}>
+              <TouchableOpacity onPress={() => {
+                this.setModalVisible(!this.state.modalVisible)
+              }}>
+                <Icon
+                  name={"arrow-left"}
+                  type="material-community"
+                  size={28}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style = {{ padding:10}}>
+              {this.props.content}
+            </View>
+          </View>
+        </Modal>
 
-  return (
-    <TouchableOpacity onPress={onPress} style={styles.button}>
-      <Icon
-        name={props.iconName}
-        type="material-community"
-        size={28}
-        color="#eceff1"
-      />
-    </TouchableOpacity>
-  );
-};
+        <TouchableOpacity onPress={() => {
+          this.setModalVisible(true)
+        }}>
+          <Icon
+            name={this.props.iconName}
+            type="material-community"
+            size={28}
+            color="#eceff1"
+          />
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+}
 
 export { HeaderButton };
--- Badmeat/src/components/common/NewEntryDialogue.js
+++ Badmeat/src/components/common/NewEntryDialogue.js
@@ -2,45 +2,16 @@
  * Created by deonj on 8/24/2017.
  */
 import React, {Component} from 'react';
-import { Modal, Text, TouchableHighlight, View } from 'react-native';
+import { Modal, Text, TouchableOpacity, View } from 'react-native';
+import {Icon} from 'react-native-elements'
 
 class NewEntryDialogue extends Component {
-  state= {
-    modalVisible: false
-  };
 
-  setModalVisible(visible) {
-    this.setState({modalVisible: visible});
-  }
 
   render() {
     return (
-      <View style={{marginTop: 22}}>
-        <Modal
-          animationType={"slide"}
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestCloe={() => {console.log("Modal has been closed")}}
-        >
-          <View style = {{marginTop: 22}}>
-            <View>
-              <Text>Ayo World!</Text>
-
-              <TouchableHighlight onPress={() => {
-                this.setModalVisible(!this.state.modalVisible)
-              }}>
-                <Text>Hide me!</Text>
-              </TouchableHighlight>
-            </View>
-          </View>
-        </Modal>
-
-        <TouchableHighlight onPress={() => {
-          this.setModalVisible(true)
-        }}>
-          <Text>Show me!</Text>
-        </TouchableHighlight>
-
+      <View style={{marginRight: 10}}>
+        <Text>ayyyyyy! Time to add some more food!</Text>
       </View>
     );
   }
