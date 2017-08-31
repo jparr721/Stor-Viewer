45c4e138287c814678c60108776fa88a12528e7b
Dante Welch <welchdan@mail.gvsu.edu>
2017-08-30 21:44:50 -0400

    Trying for camera
    Saves to camera roll, would like to not do that

--- Badmeat/android/app/src/main/java/com/badmeat/MainApplication.java
+++ Badmeat/android/app/src/main/java/com/badmeat/MainApplication.java
@@ -3,6 +3,8 @@ package com.badmeat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactlibrary.RNPdfScannerPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNPdfScannerPackage(),
+            new RCTCameraPackage(),
             new VectorIconsPackage()
       );
     }
