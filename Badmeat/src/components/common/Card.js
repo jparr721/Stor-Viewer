83ce2b6ea7f403f066787bc2ccbc5e2b422ffbce
deonj197 <deonj197@gmail.com>
2017-09-13 23:04:52 -0400

    Squashing comments

--- Badmeat/src/components/common/Card.js
+++ Badmeat/src/components/common/Card.js
@@ -23,9 +23,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 2,
     elevation: 3,
-    // marginLeft: 5,
-    // marginRight: 5,
-    // marginTop: 10,
+    
   },
 });
 
