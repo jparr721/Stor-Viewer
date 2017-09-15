b3174b28412350498019b1976b77f5407e8e7696
deonj197 <deonj197@gmail.com>
2017-09-15 02:39:11 -0400

    Placed Map component and linked dependencies
    Installed 'react-native-maps' and ran react-native link, to setup android and iOS dependencies within the project codebase. Deployed to android and tested map.

--- Badmeat/src/components/Map.js
+++ Badmeat/src/components/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Header, SmallHeader } from './common';
+import MapView from 'react-native-maps';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,14 +9,37 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#42a5f5',
   },
+  mapContainer: {
+    ...StyleSheet.absoluteFillObject,
+    height: null,
+    width: null,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
 });
 
 class Map extends Component {
   render() {
+    const { region } = this.props;
+    console.log(region);
+
     return (
       <View style={styles.container}>
-        <Header headerText="Map" />
-        <SmallHeader headerText="Place holder for Map"/>
+        <View style ={styles.mapContainer}>
+          <MapView
+            style={styles.map}
+            region={{
+              latitude: 42.331429,
+              longitude: -83.045753,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            }}
+          >
+          </MapView>
+        </View>
       </View>
     );
   }
